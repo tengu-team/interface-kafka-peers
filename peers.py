524f894b954d050a6e4f30b6b5bb66c3c1adb6d4
sborny <sander.borny@ugent.be>
2019-01-10 15:11:53 +0100

    first commit

---
+++ peers.py
@@ -0,0 +1,13 @@
+from charms.reactive import Endpoint
+
+
+class KafkaPeers(Endpoint):
+
+    def kafka_broker_count(self):
+        """
+        Counts number of kafka brokers by counting 
+        peer relations.
+
+        broker_count = #peers_relations + 1
+        """
+        return len(self.relations) + 1 
